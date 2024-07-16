4a818e033daed8c53ac3fb47c9821d9fd2817854
ChonkySeals <trivm182003@gmail.com>
2024-07-16 23:59:30 +0700

    change text

--- lib/screens/home/components/special_offers.dart
+++ lib/screens/home/components/special_offers.dart
@@ -167,7 +167,7 @@ class SpecialOfferCard extends StatelessWidget {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        TextSpan(text: "$numOfBrands Brands")
+                        TextSpan(text: "$numOfBrands Sets")
                       ],
                     ),
                   ),
